23f91933030ad1178af64df3b536ef0efd79ad4d
Fukuda Atsuo <a.tuuus.kod.f@gmail.com>
2020-03-06 17:28:16 +0900

    first commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float speed = 0;
+    private Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - startPos.x;
+            this.speed = swipeLength / 500.0f;
+            GetComponent<AudioSource>().Play();
+        }
+        this.transform.Translate(this.speed, 0, 0);
+
+        this.speed *= 0.98f;
+    }
+}
